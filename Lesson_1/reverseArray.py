0096c60f97058760ffc925e980439ffcdaf640a9
Tyrone <trorains4@gmail.com>
2021-05-16 18:54:19 +0300

    Reversal Array for question 1

--- Lesson_1/reverseArray.py
+++ Lesson_1/reverseArray.py
@@ -1,3 +1,11 @@
+# . Reverse Array In-place
+# Write a function reverseArray(A) that takes in an array A and reverses it, without using another array or collection data structure; in-place.
+
+# Example:
+
+# A = [10, 5, 6, 9] reverseArray(A) A // [9, 6, 5, 10]
+# Share the Github link to your solution.
+
 def reverseArray(lst):
     reversal = lst[::-1]
     return reversal
