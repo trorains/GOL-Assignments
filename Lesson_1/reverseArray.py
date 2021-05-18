59c5768bd4836af569ca9614f57e15c5680d5327
Tyrone <trorains4@gmail.com>
2021-05-18 13:27:04 +0300

    Changes to reverseArray

--- Lesson_1/reverseArray.py
+++ Lesson_1/reverseArray.py
@@ -6,11 +6,30 @@
 # A = [10, 5, 6, 9] reverseArray(A) A // [9, 6, 5, 10]
 # Share the Github link to your solution.
 
-def reverseArray(lst):
-    reversal = lst[::-1]
-    return reversal
+# def reverseArray(lst):
+#     reversal = lst[::-1]
+#     return reversal
     
 
 
-names = [1, 2, "Lewis", "Peter"]
+# names = [1, 2, "Lewis", "Peter"]
+# print(reverseArray(names))
+
+def reverseArray(lst):
+    beginning = 0
+    end = len(lst)-1
+
+
+    while(beginning < end):
+        # temp = lst[beginning]
+        # lst[beginning] = lst[end]
+        # lst[end] = temp
+        lst[beginning],lst[end] = lst[end],lst[beginning]
+        beginning = beginning+1
+        end = end -1
+    return lst
+    
+
+
+names = [1, 2, 3, 4]
 print(reverseArray(names))
